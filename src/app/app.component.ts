3e241c1b4d00a2455d4cee220e19b194d3c5ef16
Aliaksandr Papko <aliaksandr.papko@gmail.com>
2022-04-14 18:47:41 +0200

    feat: add `postMessage`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, map, Observable, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iframe';
+
+  #parentWindow = window.parent
+
+  sent$: Observable<string> = interval(1000).pipe(
+    map(time => {
+      const message = `Message from iframe: ${time}`;
+      this.#parentWindow.postMessage(message /*todo: targetOrigin*/);
+      return message;
+    })
+  );
+
+  ngOnInit(): void {  }
 }
