530274935e365fee240a59720f9738f2f162c1fd
Aliaksandr Papko <aliaksandr.papko@gmail.com>
2022-04-15 16:05:10 +0200

    feat: add action 'Send'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validator, Validators } from '@angular/forms';
 import { interval, map, Observable, tap, timer } from 'rxjs';
 
 @Component({
@@ -7,19 +8,31 @@ import { interval, map, Observable, tap, timer } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  control = new FormControl(null, Validators.required);
+
   title = 'iframe';
+
+  sent: string[] = [];
 
   #parentWindow = window.parent
 
-  sent$: Observable<string> = interval(1000).pipe(
+
+
+  /*sent$: Observable<string> = interval(1000).pipe(
     map(time => {
       const message = `Message from iframe v0.0.3: ${time}`;
-      this.#parentWindow.postMessage(message, '*' /*todo: targetOrigin*/);
+      this.#parentWindow.postMessage(message, '*' /!*todo: targetOrigin*!/);
       return message;
     })
-  );
+  );*/
 
   ngOnInit(): void {
     console.warn({parent: this.#parentWindow})
+  }
+
+  onSend(): void {
+    const message = this.control.value;
+    this.sent = [message, ...this.sent];
+    this.#parentWindow.postMessage(message, '*' /*todo: targetOrigin*/);
   }
 }
