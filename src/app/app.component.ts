bf894b6d02baf4ebef8cc6c07a76eab08e52e7cc
Aliaksandr Papko <aliaksandr.papko@gmail.com>
2022-04-15 16:52:47 +0200

    feat: add object message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validator, Validators } from '@angular/forms';
-import { interval, map, Observable, tap, timer } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   control = new FormControl(null, Validators.required);
 
-  title = 'iframe';
+  sent: { machineId: number }[] = [];
 
-  sent: string[] = [];
-
-  #parentWindow = window.parent
-
-
-
-  /*sent$: Observable<string> = interval(1000).pipe(
-    map(time => {
-      const message = `Message from iframe v0.0.3: ${time}`;
-      this.#parentWindow.postMessage(message, '*' /!*todo: targetOrigin*!/);
-      return message;
-    })
-  );*/
-
-  ngOnInit(): void {
-    console.warn({parent: this.#parentWindow})
-  }
+  #parentWindow = window.parent;
 
   onSend(): void {
-    const message = this.control.value;
+    const message = { machineId: this.control.value };
     this.sent = [message, ...this.sent];
-    this.#parentWindow.postMessage(message, '*' /*todo: targetOrigin*/);
+    this.#parentWindow.postMessage(
+      message,
+      '*' /*todo: targetOrigin*/
+    );
   }
 }
